4cec3bc4bbe041feb392e972b067e9e9564a4c9c
Lúcás Críostóir Meier <lucas@cronokirby.com>
2022-06-16 22:09:36 +0200

    Add basic documentation for OrDLogProver
    The verify function might change if we create smaller signatures

--- src/sigma.rs
+++ src/sigma.rs
@@ -3,21 +3,37 @@ use curve25519_dalek::ristretto::RistrettoPoint;
 use curve25519_dalek::scalar::Scalar;
 use rand_core::{CryptoRng, RngCore};
 
+/// A sigma protocol for proving knowledge of one of two discrete logarithms.
+/// 
+/// The idea is that there's two public points `big_x0`, and `big_x1`, and you
+/// want to prove that you know either `x0` such that `x0 * G = big_x0` or
+/// `x1` such that `x1 * G = big_x1`.
+/// 
+/// In practice this struct, for the prover side, always works as if you know
+/// `x0`. To prove the other side, you instead need to swap the arguments and
+/// results of the methods.
 pub struct OrDLogProver {
+    /// The discrete logarithm we know.
     x0: Scalar,
+    /// The random nonce we use.
     k0: Scalar,
+    /// We simulate a transcript for the discrete logarithm we don't know.
     fake_big_k1: RistrettoPoint,
     fake_e1: Scalar,
     fake_s1: Scalar,
 }
 
 impl OrDLogProver {
+    /// Create a new instance of the prover.
+    /// 
+    /// We use the discrete logarithm we know, and the point where we don't.
     pub fn create<R: RngCore + CryptoRng>(
         rng: &mut R,
         x0: Scalar,
         big_x1: &RistrettoPoint,
     ) -> Self {
         let k0 = Scalar::random(rng);
+        // Simulate a fake transcript for the discrete logarithm we don't know.
         let fake_e1 = Scalar::random(rng);
         let fake_s1 = Scalar::random(rng);
         let fake_big_k1 =
@@ -31,11 +47,17 @@ impl OrDLogProver {
         }
     }
 
+    /// Calculate the commitment, or first message of the sigma protocol.
+    /// 
+    /// The tuple should be flipped if we know the second point instead.
     pub fn commit(&self) -> (RistrettoPoint, RistrettoPoint) {
         let big_k0 = &self.k0 * &RISTRETTO_BASEPOINT_TABLE;
         (big_k0, self.fake_big_k1)
     }
 
+    /// Calculate the response to the challenge.
+    /// 
+    /// The two response tuples should be flipped if we know the second point instead.
     pub fn respond(&self, e: &Scalar) -> ((Scalar, Scalar), (Scalar, Scalar)) {
         let e0 = e - self.fake_e1;
         let s0 = self.k0 - e0 * self.x0;
